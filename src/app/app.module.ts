8c51ab3f16c8146a685f081c589495c10c8879dd
Luis Aguirre <luis.aguirre@hovahealth.com>
2018-10-22 18:29:46 -0500

    cambios iniciales

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    InicioComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
