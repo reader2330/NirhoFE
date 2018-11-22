f97af675e5b5e95cccb9a6de40f79e1ff42cba95
Marcello Montenegro <marcevalmon@icloud.com>
2018-11-22 00:37:26 -0600

    Finalización de asignación de encuesta

--- src/app/modules/eva/components/sidebar-360/sidebar-360.component.ts
+++ src/app/modules/eva/components/sidebar-360/sidebar-360.component.ts
@@ -20,8 +20,8 @@ import {
 })
 export class Sidebar360Component implements OnInit {
   mobile = false;
-  selectedItem = 2;
-  selectModule = 2;
+  selectedItem = 3;
+  selectModule = 3;
   modules = [];
   user = {};
   constructor(breakpointObserver: BreakpointObserver, private route: Router, private loginService: LoginService) {
